d2e95592fa64c6a3525adc09f62b6ffa0956ea42
Incarnation P. Lee <incarnation.p.lee@gmail.com>
2015-04-06 17:00:08 +0800

    Update AT&T assembly code. Bug fix     1. $label for the address of the location     2. . for current instruction address     3. update gitignore

--- boot/boot_sample.s
+++ boot/boot_sample.s
@@ -1,30 +1,34 @@
-.code16
 .section .text
 .globl _start
+.code16
 _start:
            mov     %cs, %ax
            mov     %ax, %ds
            mov     %ax, %es
-           call    dispstring
-           jmp     _start
 
-dispstring:
-           mov     bootmsg, %ax
-           mov     %ax, %bp
-           mov     $0x10, %cx
-           mov     $0x1301, %ax
-           mov     $0x00c, %bx
-           mov     $0, %dl
+           mov     $bootmsg, %bp
+           mov     msg_len, %cx
+           mov     $0x13, %ah
+           mov     $0x1, %al
+           mov     $0x01, %bl
+           mov     $0x0, %bh
+           # location (dh, dl)
+           mov     $2, %dl
+           mov     $2, %dh
            int     $0x10
-           ret
-.section .data
+
+           jmp     .
 bootmsg:
-           .ascii      "Hello, OS world!"
+           .asciz      "Hello, Incarnation P. Lee, this is the OS world!"
+msg_len:
+           .int        . - bootmsg
+           .org        0x1fe, 0x90
            .short      0xaa55
 # command reference:
 #     as -o boot.o boot_sample.s
-#     ld --oformat binary -Ttext 7c00 -Tdata 7dee -o boot
+#     ld --oformat binary -Ttext 7c00 -N -o boot
 #     dd if=./boot/boot.bin of=/dev/fd0 bs=512 count=1
+#     dd if=./boot/boot.bin of=boot.img bs=512 count=1
 # if use hard disk, of=/dev/sdb instead of sdb1
 #     write the boot.bin file to hard disk,
 #     not partition(sdb1) of hard disk.
